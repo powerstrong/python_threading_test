1a2c9e7833c680ce6af456575a3af0b97d425f53
jslee <jslee@plaif.com>
2023-07-16 13:48:15 +0900

    improve: feat code by pylint rules

--- pythonthreadtest.py
+++ pythonthreadtest.py
@@ -1,32 +1,34 @@
+"""test python threading module"""
+
 import threading
 import subprocess
 import time
 
 class PythonThreadingTest:
-    def main(self):
+    """main class"""
+
+    def __init__(self):
         self.is_generating = True
 
         thread1 = threading.Thread(target=self.crawl_something_periodically)
         thread1.start()
         for i in range(5):
-            print('[main] loop {}'.format(i))
+            print(f'[main] loop {i}')
             self.run_subprocess()
 
         self.is_generating = False
         thread1.join()
         print('[main] finished!')
 
-    # 한번 실행되면 한참동안 소식이 없어지는 서브프로세스
     def run_subprocess(self):
-        subprocess.run(["python", "threesecsleeper.py"])
+        """한번 실행되면 한참동안 소식이 없어지는 서브프로세스"""
+        subprocess.run(["python", "threesecsleeper.py"], check=False)
 
-    # 주기적으로 뭔가 일을 뚝딱뚝딱..
     def crawl_something_periodically(self):
-        while(self.is_generating):
+        """주기적으로 뭔가 일을 뚝딱뚝딱.."""
+        while self.is_generating:
             print(' [main] crawl_something_periodically()')
             time.sleep(1)
-        pass
-
 
 if __name__ == '__main__':
-    PythonThreadingTest().main()
+    PythonThreadingTest()
--- threesecsleeper.py
+++ threesecsleeper.py
@@ -1,3 +1,4 @@
+"""3초 동안 작업을 하고 꺼지는 모듈"""
 import time
 
 print(' [sub] started')
